9e7ba8f346554eefc29a08398c8d1bc9cbd4bd44
Ganesh Thanu <93513715+rationalyt@users.noreply.github.com>
2022-05-10 03:18:24 -0400

    Refactoring to an OOPS model

--- tictactoe.py
+++ tictactoe.py
@@ -1,132 +1,115 @@
-a = [7,8,9]
-b = [4,5,6]
-c = [1,2,3]
+class Board:
 
-
-def display_board():
-    for x in a:
-        print("{0}    ".format(x),end=" ")
-    print("\n")
-    for x in b:
-        print("{0}    ".format(x),end=" ")
-    print("\n")
-    for x in c:
-        print("{0}    ".format(x),end=" ")
-    print("\n")
+    def __init__(self):
+        self.board = [[1,2,3],[4,5,6],[7,8,9]]
+        self.player_1,self.player_2 = "X","O"   
     
+    def display(self):
+        print("Player 1 is X")
+        print("Player 2 is O\n")
+        for r in self.board:
+            print()
+            for c in r:
+                print(c, end = " ")
+        print("\n")
 
-def check_rows():
-    if (a[0] == a[1] and a[1]==a[2]):
-        return True
-    elif (b[0]==b[1] and b[1]==b[2]):
-        return True
-    elif (c[0]==c[1] and c[1]==c[2]):
-        return True
-    else:
+    def take_input(self,c):  
+        row,col,move = 0,0,0   
+        print(f"\nYour Move Player {c}:")
+        print("\n")
+        while(1):
+            self.display()
+            move = input()
+            if not move.isdigit():
+                print(f"\nMove Invalid Player {c}")
+                continue
+            move = int(move)
+            if move < 1 or move > 9: 
+                print("\nMove Invalid")
+                continue
+            if move > 0 and move < 4: row = 1
+            if move > 3 and move < 7: row = 2
+            if move > 6 and move < 10: row = 3
+            col = move - (3*row - 2)
+            if isinstance(self.board[row-1][col],str):
+                print("\nMove already taken! Choose another")
+                continue
+            break
+        if c == 1: self.board[row-1][col] = self.player_1
+        else : self.board[row-1][col] = self.player_2
+        return (row-1,col,c)           
+
+  
+class Game(Board):
+
+    def __init__(self):
+        super().__init__()
+        self.row = 0
+        self.col = 0
+        self.player = 0
+        self.value = 0
+
+    def check_board(self):
+        for r in self.board:
+            for b in r:
+                if isinstance(b,int) : return True
+        print(f"\nGame Draw\n")
         return False
 
-def check_cols():
-
-    if(a[0]==b[0] and b[0]==c[0]):
+    def check_row(self):        
+        r,c = self.row,self.col
+        while c < 3:
+            if self.board[r][c] != self.value : return False
+            c+=1
+        c = self.col
+        while c >= 0:
+            if self.board[r][c] != self.value : return False
+            c -= 1
+        print(f"\nPlayer {self.player} Won\n")
         return True
-    elif(a[1]==b[1] and b[1]==c[1]):
+
+    def check_col(self):
+        r, c = self.row, self.col
+        while r < 3:
+            if self.board[r][c] != self.value : return False
+            r += 1
+        r = self.row
+        while r >= 0:
+            if self.board[r][c] != self.value : return False
+            r -= 1
+        print(f"\nPlayer {self.player} Won\n")
         return True
-    elif(a[2]==b[2] and b[2]==c[2]):
+
+    def check_diag(self):
+        diag1 = str(self.board[0][0]) + str(self.board[1][1]) + str(self.board[2][2])
+        diag2 = str(self.board[0][2]) + str(self.board[1][1]) + str(self.board[2][0])
+        d1 = all(elem == diag1[0] for elem in diag1)
+        d2 = all(elem == diag2[0] for elem in diag2)
+        if d1 or d2 : 
+            print(f"\nPlayer {self.player} Won\n")
+            return True        
+        return False 
+
+    def check_game(self):
+        if not self.check_board() : return False
+        self.value = self.board[self.row][self.col]
+        if self.check_row(): return False
+        if self.check_col(): return False
+        if self.row + self.col == 2 or self.row == self.col: 
+            if self.check_diag(): return False
         return True
-    else:
-        return False
+        
 
-def check_diag():
-
-    if a[0] == b[1] and b[1] == c[2]:
-        return True
-    elif a[2] == b[1] and b[1] == c[0]:
-        return True
-    else:
-        return False
-
-def check_space():
-    for x in a:
-        if isinstance(x,int) == True:
-            return False
-    for x in b:
-        if isinstance(x,int) == True:
-            return False
-    for x in c:
-        if isinstance(x,int) == True:
-            return False
-    return True
-
-def check_game_on(input,coin):
-    if input in a:
-        a[a.index(input)] = coin
-    if input in b:
-        b[b.index(input)] = coin
-    if input in c:
-        c[c.index(input)] = coin
-    game_check = check_rows()
-    if game_check == False:
-     game_check = check_cols()
-    if game_check == False:
-     game_check = check_diag()
-    if game_check == True:
-        return "FalseWin"
-    else:    
-        if(check_space() == False):
-            return "True"
-        else:
-            return "FalseDraw"
- 
-
-def main():
-    game_on = "True"
-    print("")
-    while game_on == "True":
-     display_board()
-     input1_check = False
-     while input1_check == False:
-      print("Player 1 input:")
-      input1 = int(input())
-      if input1 not in range(1,10):
-          print("Invalid value")
-          continue
-      else:
-          input1_check = True
-          game_on = check_game_on(input1,'X')
-     if game_on=="True":
-         display_board()
-         print("Player 2 input:")
-         input2_check = False
-         while input2_check == False:
-          input_2 = int(input())
-          if input_2 not in range(1,10):
-           print("Invalid value")
-           continue
-          else:
-           input2_check = True
-           game_on = check_game_on(input_2,'O')
-           if game_on=="FalseWin":
-               display_board()
-               print("Player 2 Won")
-               break
-           elif game_on=="FalseDraw":
-               display_board()
-               print("Its a draw")
-               break
-     elif game_on=="FalseWin":
-         display_board()
-         print("Player 1 Won")
-         break
-     elif game_on=="FalseDraw":
-         display_board()
-         print("Its a Draw")
-         break
-
-if __name__ == "__main__":
-    main()
+g = Game()
+while True:
+    r,c,p = g.take_input(1)
+    g.row = r
+    g.col = c
+    g.player = p
+    if not g.check_game(): break
     
-    
-
-
-
-    
+    r, c, p = g.take_input(2)
+    g.row = r
+    g.col = c
+    g.player = p
+    if not g.check_game(): break
